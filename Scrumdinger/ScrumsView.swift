3eace782a41ea18c0a38715d86663361a0b5e52d
Rafal Granoszewski <rgranoszewski@gmail.com>
2023-01-11 22:12:44 +0100

    Commit

--- Scrumdinger/ScrumsView.swift
+++ Scrumdinger/ScrumsView.swift
@@ -9,10 +9,11 @@ import SwiftUI
 
 struct ScrumsView: View {
     let scrums: [DailyScrum]
+    
     var body: some View {
         List {
             ForEach(scrums) { scrum in
-                NavigationLink(destination: Text(scrum.title)) {
+                NavigationLink(destination: DetailView(scrum: scrum)) {
                     CardView(scrum: scrum)
                 }
                     .listRowBackground(scrum.theme.mainColor)
